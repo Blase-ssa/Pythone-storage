aec91828b248bf5571a924a70ad0977c9f3231da
Sergei Silantev <61013982+Blase-ssa@users.noreply.github.com>
2024-11-10 16:17:07 +0100

    Add AWS CKD + Lambda workshop

---
+++ AWS_CDK_LAMBDA_workshop/cdk_workshop/cdk_workshop_stack.py
@@ -0,0 +1,43 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
+)
+
+from cdk_dynamo_table_view import TableViewer
+from hitcounter import HitCounter
+
+class CdkWorkshopStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_10,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler',
+        )
+
+        # apigw.LambdaRestApi(
+        #     self, 'Endpoint',
+        #     handler=my_lambda,
+        # )
+
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda,
+        )
+
+        apigw.LambdaRestApi(
+            self, 'EndpointWithCounter',
+            handler=hello_with_counter._handler,
+        )
+
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table,
+        )
